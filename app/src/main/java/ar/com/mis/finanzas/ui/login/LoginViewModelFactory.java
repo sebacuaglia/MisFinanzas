73597441b12c4be653891f0bba5dfce819a3a78b
sebacuaglia <sebacuaglia@gmail.com>
2022-03-09 20:06:38 -0300

    primer commit

---
+++ app/src/main/java/ar/com/mis/finanzas/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package ar.com.mis.finanzas.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import ar.com.mis.finanzas.data.LoginDataSource;
+import ar.com.mis.finanzas.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
